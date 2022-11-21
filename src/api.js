5187a807c34d2c9361fc437c9526ba0da1ee7910
Fpitkat <75321987+Fpitkat@users.noreply.github.com>
2022-11-21 13:21:02 -0500

    SearchBar component

--- src/api.js
+++ src/api.js
@@ -4,13 +4,13 @@
 import axios from 'axios'
 const url = 'https://api.unsplash.com/search/photos'
 
-const searchImages = async () => {
+const searchImages = async (term) => {
   const response = await axios.get(url, {
     headers: {
       Authorization: 'Client-ID R4s8cBVQz-LMwACgmuw8YdsufdGPNifXeUz-cTxSHRk',
     },
     params: {
-      query: 'cars',
+      query: term,
     },
   })
   return response.data.results
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,31 @@
-function SearchBar() {
-  return <div>SearchBar</div>
+import { useState } from 'react'
+
+function SearchBar({ onSubmit }) {
+  const [term, setTerm] = useState('')
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault()
+    onSubmit(term)
+  }
+
+  const handleChange = (event) => {
+    setTerm(event.target.value)
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleFormSubmit}>
+        <input
+          type='text'
+          value={term}
+          onChange={handleChange}
+          placeholder='Search unSplash!'
+        />
+        {/* I ADDED THIS FEATURE */}
+        {term.length < 2 && <p>Term must be contain more than 2 characters.</p>}
+      </form>
+    </div>
+  )
 }
 
 export default SearchBar
